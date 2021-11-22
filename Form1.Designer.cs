b52a6a08656dbf5eb4d18565b4108a308b964f61
Attilio Sicuro <attilio.sicuro@sincon.it>
2021-11-22 15:21:44 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Esportatore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TV = new System.Windows.Forms.TreeView();
+            this.FB = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flavio = new System.Windows.Forms.PictureBox();
+            this.cmbConfigurazione = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.ballon = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMessaggio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.flavio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TV
+            // 
+            this.TV.CheckBoxes = true;
+            this.TV.Location = new System.Drawing.Point(12, 278);
+            this.TV.Name = "TV";
+            this.TV.Size = new System.Drawing.Size(625, 305);
+            this.TV.TabIndex = 0;
+            this.TV.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TV_NodeMouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 599);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Esporta File Selezionati";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flavio
+            // 
+            this.flavio.BackColor = System.Drawing.Color.Transparent;
+            this.flavio.Image = ((System.Drawing.Image)(resources.GetObject("flavio.Image")));
+            this.flavio.Location = new System.Drawing.Point(12, 84);
+            this.flavio.Name = "flavio";
+            this.flavio.Size = new System.Drawing.Size(130, 188);
+            this.flavio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.flavio.TabIndex = 2;
+            this.flavio.TabStop = false;
+            // 
+            // cmbConfigurazione
+            // 
+            this.cmbConfigurazione.FormattingEnabled = true;
+            this.cmbConfigurazione.Location = new System.Drawing.Point(264, 251);
+            this.cmbConfigurazione.Name = "cmbConfigurazione";
+            this.cmbConfigurazione.Size = new System.Drawing.Size(373, 21);
+            this.cmbConfigurazione.TabIndex = 3;
+            this.cmbConfigurazione.SelectedIndexChanged += new System.EventHandler(this.cmbConfigurazione_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 599);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Esporta e trasferisci file selezionati";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 7000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ambiente";
+            // 
+            // ballon
+            // 
+            this.ballon.BackColor = System.Drawing.Color.Transparent;
+            this.ballon.Image = ((System.Drawing.Image)(resources.GetObject("ballon.Image")));
+            this.ballon.Location = new System.Drawing.Point(112, 12);
+            this.ballon.Name = "ballon";
+            this.ballon.Size = new System.Drawing.Size(546, 199);
+            this.ballon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ballon.TabIndex = 6;
+            this.ballon.TabStop = false;
+            this.ballon.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblMessaggio
+            // 
+            this.lblMessaggio.AcceptsReturn = true;
+            this.lblMessaggio.AcceptsTab = true;
+            this.lblMessaggio.BackColor = System.Drawing.Color.White;
+            this.lblMessaggio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lblMessaggio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessaggio.Location = new System.Drawing.Point(210, 59);
+            this.lblMessaggio.Multiline = true;
+            this.lblMessaggio.Name = "lblMessaggio";
+            this.lblMessaggio.ReadOnly = true;
+            this.lblMessaggio.Size = new System.Drawing.Size(358, 78);
+            this.lblMessaggio.TabIndex = 8;
+            this.lblMessaggio.Visible = false;
+            this.lblMessaggio.EnabledChanged += new System.EventHandler(this.lblMessaggio_EnabledChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 630);
+            this.Controls.Add(this.lblMessaggio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cmbConfigurazione);
+            this.Controls.Add(this.flavio);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TV);
+            this.Controls.Add(this.ballon);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SVN E\' MEGLIO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.flavio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TV;
+        private System.Windows.Forms.FolderBrowserDialog FB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox flavio;
+        private System.Windows.Forms.ComboBox cmbConfigurazione;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ballon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox lblMessaggio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WinSCP;
+
+namespace Esportatore
+{
+    public partial class Form1 : Form
+    {
+        Configurazione[] configurazioni = null;
+        int confSelezionata = -1;
+        string selectedPath = "";
+        string server = "";
+        string user = "";
+        string pwd = "";
+        string fingerprint = "";
+        string[] radiceIstanze = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void scanIniFile(string iniFile)
+        {
+            string[] parts = iniFile.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 5)
+                return;
+
+            server = parts[0];
+            user = parts[1];
+            pwd = parts[2];
+            fingerprint = parts[3];
+            radiceIstanze = parts[4].Replace("[","").Replace("]","").Split(new String[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+        }
+        private void ShowBallonMessage(string message)
+        {
+            string[] precoda = new string[]
+            {
+                "Guarda non so come dirtelo, ma ",
+                "Lo sapevi che ",
+                "Devo dirti una cosa: "
+            };
+            string[] accoda = new string[]
+            {
+                "...\r\n Questo è molto INQUIETANTE!",
+                ". \r\nSai, sto costruendo un Barchino Divergente!",
+                "; \r\nCerto che scoccy è una checca isterica!",
+                "... \r\nVorrei essere un insegnante",
+                ". \r\nSapete quanto guadagna un muratore?!?",
+                "...\r\nDevo andare in Norvegia... Lì tutti lavorano il legno..."
+            };
+            lblMessaggio.Text = precoda[(new Random(DateTime.UtcNow.Millisecond)).Next(precoda.Length)] + message + accoda[(new Random(DateTime.UtcNow.Millisecond)).Next(accoda.Length)];
+            ballon.Visible = lblMessaggio.Visible = true;
+            timer1.Enabled = true;
+            timer1.Interval = 12000;
+            timer1.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string iniFile = Application.StartupPath + "\\configurazioni.ini";
+
+                StreamReader file = new StreamReader(iniFile);
+                string contentIniFile = file.ReadToEnd();
+                file.Close();
+                configurazioni = Configurazione.getConfigurazioni(contentIniFile);
+                if (configurazioni == null)
+                {
+                    ShowBallonMessage("Non posso aprire il file delle configurazioni");
+                    //MessageBox.Show("Impossibile importare configurazioni.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    foreach (Configurazione c in configurazioni)
+                        cmbConfigurazione.Items.Add(c.nome);
+                }
+            }
+            catch(Exception ex)
+            {
+                ShowBallonMessage(ex.Message);
+            }
+
+        }
+        private void popolaTree(TreeNode node, string thePath)
+        {
+            if (thePath != "")
+            {
+                string[] allDirs = Directory.GetDirectories(thePath);
+                if (allDirs.Length > 0)
+                {
+                    foreach (string s in allDirs)
+                    {
+                        
+                        TreeNode tn = new TreeNode(s.Replace(thePath + "\\", ""));
+                        DateTime data = File.GetLastWriteTime(s);
+                        
+                        tn.ToolTipText = " modificato: " + data.ToShortDateString() + " " + data.ToShortTimeString();
+                        tn.ImageKey = "dir";
+                        tn.Tag = s;
+                        node.Nodes.Add(tn);
+                        popolaTree(tn, s);
+                    }
+                }
+                string[] all = Directory.GetFiles(thePath);
+                if (all.Length > 0)
+                {
+                    foreach (string s in all)
+                    {
+                        TreeNode tn = new TreeNode(s.Replace(thePath + "\\", ""));
+                        tn.Tag = s;
+                        DateTime data = File.GetLastWriteTime(s);
+                        tn.ToolTipText += " modificato: " + data.ToShortDateString() + " " + data.ToShortTimeString();
+                        node.Nodes.Add(tn);
+                    }
+                }
+
+            }
+        }
+        private void checkNodeBackOnly(bool status, TreeNode node)
+        {
+            node.Checked = status;
+            if (node.Parent != null)
+            {
+                TreeNodeCollection siblings = node.Parent.Nodes;
+                bool canDoCheck = true;
+                foreach (TreeNode t in siblings)
+                    if (t != node && t.Checked && !status)
+                        canDoCheck = false;
+                if (canDoCheck)
+                    checkNodeBackOnly(status, node.Parent);
+                
+            }
+        }
+        private void checkNodeForward(bool status, TreeNode node, int level)
+        {
+            node.Checked = status;
+            if (node.Nodes.Count > 0 )
+                foreach (TreeNode tn in node.Nodes)
+                    checkNodeForward(status, tn, level + 1);
+        }
+        private void TV_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //if (e.Node.Tag == null) //file
+            bool status = !e.Node.Checked;
+                checkNodeBackOnly(status, e.Node);
+            if (!String.IsNullOrEmpty(e.Node.ImageKey) && e.Node.ImageKey == "dir") {
+                checkNodeForward(status, e.Node, 0);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowBallonMessage("dovresti scegliere una cartella di destinazione");
+                FB.ShowDialog();
+                string destination = FB.SelectedPath;
+                if (destination != selectedPath)
+                {
+                    string theBase = destination;
+                    TreeNode root = TV.Nodes[0];
+                    foreach (TreeNode tn in root.Nodes)
+                    {
+                        creaTree(tn, theBase);
+
+                    }
+                }
+                ShowBallonMessage("l'operazione è terminata con successo!");
+
+            }
+            catch (Exception ex)
+            {
+                ShowBallonMessage(ex.Message);
+            }
+        }
+
+        private void creaTree(TreeNode t, string basePath)
+        {
+            string path = basePath + "\\" + t.Text;
+            if (t.Checked)
+            {
+                if (String.IsNullOrEmpty(t.ImageKey))
+                {
+                    //è un file, creo il percorso con la directory
+                    Directory.CreateDirectory(basePath);
+                    File.Copy(t.Tag.ToString(), path);
+                }
+                else
+                {
+                    foreach (TreeNode tn in t.Nodes)
+                        creaTree(tn, path);
+
+                }
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            FB.ShowDialog();
+            selectedPath = FB.SelectedPath;
+            TreeNode radice = TV.Nodes.Add("*root*");
+
+            popolaTree(radice, selectedPath);
+        }
+
+        private void TrasferisciFiles()
+        {
+            if (configurazioni != null && confSelezionata != -1)
+            {
+                Configurazione c = configurazioni[confSelezionata];
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = c.server,
+                    UserName = c.user,
+                    Password = c.pwd,
+                    SshHostKeyFingerprint = c.fingerprint
+                };
+
+                string destination = Application.StartupPath + "\\tmp\\";
+                try
+                {
+                    Directory.Delete(destination, true);
+                }
+                catch(Exception ex)
+                {
+                }
+                //creo la directory tmp se non esistente
+                Directory.CreateDirectory(destination);
+                try
+                {
+                    if (destination != selectedPath)
+                    {
+                        string theBase = destination;
+                        TreeNode root = TV.Nodes[0];
+                        foreach (TreeNode tn in root.Nodes)
+                        {
+                            creaTree(tn, theBase);
+
+                        }
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+                try
+                {
+                    using (Session session = new Session())
+                    {
+                        // Connect
+                        session.Open(sessionOptions);
+                        TransferOptions transferOptions = new TransferOptions();
+                        transferOptions.TransferMode = TransferMode.Binary;
+                        foreach (string radiceIstanza in c.radiceIstanze)
+                        {
+                            TransferOperationResult transferResult;
+                            // Upload files
+                            transferResult = session.PutFiles(destination + "*", radiceIstanza + "/*", false, transferOptions);
+                            transferResult.Check();
+                        }
+                    }
+                }
+                catch (Exception ex) { ShowBallonMessage(ex.Message); }
+
+                Directory.Delete(destination, true);
+                ShowBallonMessage("l'operazione è terminata con successo!");
+            }
+            else
+                ShowBallonMessage("c'è un problema: devi selezionare un ambiente su cui portare i file. Stai attento, che Michela Brivido si arrabbia.");
+        }
+
+        private void cmbConfigurazione_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            confSelezionata = cmbConfigurazione.SelectedIndex;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowBallonMessage("questa è un'operazione inqueitante: sto per portare i files!");
+            TrasferisciFiles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ballon.Visible = lblMessaggio.Visible = false;
+            timer1.Enabled = false;
+        }
+
+        private void lblMessaggio_EnabledChanged(object sender, EventArgs e)
+        {
+            lblMessaggio.BackColor = Color.White;
+        }
+    }
+
+    /**
+     * 
+     *FILE DI CONFIGURAZIONE configurazioni.ini:
+     *
+     *NOME AMBIENTE
+     *indirizzo (ip/URL)
+     *username
+     *password
+     *fingerprint
+     *[istanze,separate,da,virgola]
+     * carattere "-" per 133 volte (funge da separatore)
+     */
+    public class Configurazione
+    {
+        public string nome = "";
+        public string server = "";
+        public string user = "";
+        public string pwd = "";
+        public string fingerprint = "";
+        public string[] radiceIstanze = null;
+
+        public static Configurazione[] getConfigurazioni(string confList)
+        {
+            try
+            {
+                List<Configurazione> configs = new List<Configurazione>();
+
+                string[] confs = confList.Split(new String[] { "".PadLeft(133, '-') }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string conf in confs)
+                {
+                    string[] righe = conf.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    if (righe.Length == 6)
+                    {
+                        Configurazione c = new Configurazione();
+                        c.nome = righe[0];
+                        c.server = righe[1];
+                        c.user = righe[2];
+                        c.pwd = righe[3];
+                        c.fingerprint = righe[4];
+                        c.radiceIstanze = righe[5].Replace("[", "").Replace("]", "").Split(new String[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                        configs.Add(c);
+                    }
+
+                }
+                return configs.ToArray();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+
+}
